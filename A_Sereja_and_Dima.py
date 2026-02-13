71d1b4a4ea1e8683e30533cdbad386983583a944
desu <desalegntressa@gmail.com>
2026-02-13 09:16:23 -0800

    A_Sereja_and_Dima

---
+++ A_Sereja_and_Dima.py
@@ -0,0 +1,31 @@
+n = int(input())
+lst = list(map(int, input().split()))
+
+serej = 0
+dima = 0
+l = 0
+r = n-1
+i = 1
+
+while l<=r:
+    if lst[l]>lst[r]:
+        if i%2 != 0:
+            serej += lst[l]
+            l += 1
+
+        else:
+            dima += lst[l]
+            l += 1
+        i += 1
+
+    else:
+        if i%2 != 0 :
+            serej += lst[r]
+            r -= 1
+
+        else:
+            dima += lst[r]
+            r -= 1
+        i += 1
+print(serej, dima)
+
